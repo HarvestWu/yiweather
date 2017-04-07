160d46ca5c8231eca87edad0ced066eb944a88f8
HarvestWu <672341573@qq.com>
2017-04-07 17:01:01 +0800

    加入显示天气的功能

--- app/src/main/java/com/yiweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/yiweather/android/ChooseAreaFragment.java
@@ -7,6 +7,7 @@ package com.yiweather.android;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -85,6 +86,12 @@ public class ChooseAreaFragment extends Fragment{
                 }else if(currentLevel==LEVEL_CITY){
                     selectedCity=cityList.get(position);
                     queryCounties();
+                }else if (currentLevel==LEVEL_COUNTY){
+                    String weatherId=countyList.get(position).getWeatherId();
+                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/com/yiweather/android/util/Utility.java
+++ app/src/main/java/com/yiweather/android/util/Utility.java
@@ -6,9 +6,11 @@ package com.yiweather.android.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.yiweather.android.db.City;
 import com.yiweather.android.db.County;
 import com.yiweather.android.db.Province;
+import com.yiweather.android.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -85,6 +87,22 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
 
